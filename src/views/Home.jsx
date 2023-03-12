e640611a81f48bdaa08f714e3a3bc6242141e321
rustiandacode <rustiandazen09@gmail.com>
2023-03-12 22:33:49 +0700

    try carousel

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className="container mx-auto w-full p-8 bg-black-for-content h-screen overflow-scroll">
       <Navbar />
-      <Hero />
+      <Carousel />
       <TrendingMovies />
     </div>
   )

41ad73a857bc8ed619d057dbe970674b1f54ebe2
rustiandacode <rustiandazen09@gmail.com>
2023-03-20 02:02:41 +0700

    fixing issues : description movie card show up on mobil

--- src/components/DiscoverContent.jsx
+++ src/components/DiscoverContent.jsx
@@ -76,7 +76,7 @@ const DiscoverContent = (props) => {
           }
           return (
             <div
-              className="w-1/2 lg:w-1/5 md:w-1/4 sm:w-1/3 p-2 cursor-pointer overflow-hidden hover:scale-110 hover:duration-300 hover:z-40 group relative"
+              className="w-1/2 lg:w-1/5 md:w-1/4 sm:w-1/3 p-2 cursor-pointer overflow-hidden md:hover:scale-110 hover:duration-300 hover:z-40 group relative"
               key={movie.id}
               onClick={() => {
                 handleNavigate(movie.id)
@@ -91,7 +91,7 @@ const DiscoverContent = (props) => {
                 alt={movie.title}
                 className="rounded-lg shadow-xl"
               />
-              <div className="absolute text-white bottom-0 right-0 left-0 h-[80%] rounded-lg p-3 m-2 bg-gradient-to-t from-black to-black-for-card opacity-80 overflow-hidden translate-y-72 group-hover:-translate-y-0 group-hover:block group-hover:transition group-hover:duration-300 group-hover:ease-in-out">
+              <div className="hidden md:block md:show-card">
                 <h4 className="truncate font-semibold md:text-lg text-md">
                   {movie.title}
                 </h4>
